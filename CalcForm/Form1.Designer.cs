2a67158b813d89d50df69a7d0ab0b9a028049ff0
eqour <eqour@yandex.ru>
2022-10-08 09:41:01 +0400

    C-12 Добавил блокировку 2 поля для операций с 1 параметром

--- CalcForm/Form1.Designer.cs
+++ CalcForm/Form1.Designer.cs
@@ -79,6 +79,7 @@ namespace CalcForm
             // 
             // textBox2
             // 
+            this.textBox2.Enabled = false;
             this.textBox2.Location = new System.Drawing.Point(152, 41);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
--- CalcForm/Form1.cs
+++ CalcForm/Form1.cs
@@ -13,6 +13,8 @@ namespace CalcForm
     public partial class Form1 : Form
     {
         private Dictionary<string, Func<double, double, double>> operations;
+        private List<string> UnaryOperations;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,28 +25,33 @@ namespace CalcForm
             operations.Add("sin", (x, y) => Math.Sin(x));
             operations.Add("cos", (x, y) => Math.Cos(x));
 
+            UnaryOperations = new List<string>();
+            UnaryOperations.AddRange(new List<string>() { "sin", "cos", "tg" });
+
             list_operations.Items.Add("+");
             list_operations.Items.Add("-");
             list_operations.Items.Add("x^(1/y)");
             list_operations.Items.Add("cos");
         }
 
-
-
         private void list_operations_SelectedIndexChanged(object sender, EventArgs e)
         {
-
-           
+            textBox2.Enabled = !UnaryOperations.Contains(list_operations.SelectedItem);
         }
 
         private void btn_calculate_Click(object sender, EventArgs e)
         {
-            double operand1 = double.Parse(textBox1.Text);
+            double operand1 = ParseTextBoxValue(textBox1);
             string operation = (string)list_operations.SelectedItem;
-            double operand2 = double.Parse(textBox2.Text);
+            double operand2 = ParseTextBoxValue(textBox2);
             double result = operations[operation](operand1, operand2);
             listView1.Items.Add(result.ToString());
+        }
 
+        private double ParseTextBoxValue(TextBox textBox)
+        {
+            if (textBox.TextLength == 0) return 0;
+            else return double.Parse(textBox.Text);
         }
 
         private void btn_clear_all_Click(object sender, EventArgs e)
